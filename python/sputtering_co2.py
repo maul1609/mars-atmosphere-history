2bababdee16b19cb495653ba24780fe2c5b9e732
Paul Connolly <p.connolly@manchester.ac.uk>
2022-03-10 13:05:23 +0000

    added function to calculate co2 sputting rate using data from Luhmann et al. 1992 GRL

---
+++ python/sputtering_co2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import scipy.interpolate as scint 
+
+
+def sputtering_co2_rate(timeGyr):
+    
+    euv_grid = np.array([6., 3., 1.])
+    time_grid = np.array([1., 2., 4.5])
+    f_co2_grid = np.array([3.e26, 6.e25, 3.e25])
+    
+    # generate my interpolation function from the data points
+    euv_func=scint.interp1d(time_grid,euv_grid,fill_value='extrapolate',kind='linear')
+    f_co2_func=scint.interp1d(time_grid,f_co2_grid,fill_value='extrapolate',kind='linear')
+    
+    
+    
+    return (euv_func(timeGyr), f_co2_func(timeGyr) )
+    
+    
+if __name__ == "__main__":
+    # test the function by evaluating and plotting
+    import matplotlib.pyplot as plt 
+    
+    t_test_gyr = np.linspace(0.,5.,100)
+    
+    (euv_flux, f_co2_flux) = sputtering_co2_rate(t_test_gyr)
+    
+    plt.ion()
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(t_test_gyr,f_co2_flux)    
+    plt.xlabel('time (Gyr)')
+    plt.ylabel('F$_{CO2}$')
+    plt.subplot(212)
+    plt.plot(t_test_gyr,euv_flux)
+    plt.xlabel('time (Gyr)')
+    plt.ylabel('Relative EUV flux')
+    plt.show()
