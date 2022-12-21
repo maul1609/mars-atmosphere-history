6a6dd4521c740d8c60f94922b1429f13014957fb
Paul Connolly <p.connolly@manchester.ac.uk>
2022-12-21 18:32:34 +0000

    update the crater chronology model

--- python/crater_chronology.py
+++ python/crater_chronology.py
@@ -6,6 +6,11 @@ def N1(t):
     # input time ago (i.e. should be positive)
     return 5.44e-14*(np.exp(6.93*t)-1.)+8.38e-4*t
     
+def dN1_dt(t):
+    # see figure 1, Morbidelli et al. 2012
+    # input time ago (i.e. should be positive)
+    return 3.76992e-13*np.exp(6.93*t)+8.38e-4
+    
 def dN20_dt1(t):
     # see figure 3, Morbidelli et al. 2012
     # input time ago (i.e. should be positive)
@@ -27,25 +32,60 @@ def dN20_dt3(t):
 
     return N
 
+def dN20_dt_integrand(t,model=1):
+    if(model==1):
+        dN=dN20_dt1(t)
+    elif(model==2):
+        if(t<4.1):
+            dN=dN20_dt1(t)
+        else:
+            dN=dN20_dt2(t)
+    elif(model==3):
+        if(t<4.1):
+            dN=dN20_dt1(t)
+        else:
+            dN=dN20_dt3(t)
+    else:
+        print('bad')
+        dN=np.zeros(np.shape(t))
+           
+    return dN
+
+def N1_between(Amars,t1,t2,model=1):
+    from scipy.integrate import quad
+    result = quad(dN1_dt,t1,t2)
+    return Amars*result[0]
+
+def N20_between(Amars,t1,t2,model=1):
+    from scipy.integrate import quad
+    result = quad(dN20_dt_integrand,t1,t2,model)
+    return Amars*result[0]
 
 if __name__ == "__main__":
     from scipy.integrate import quad
     
-    
+    Rmars=3389.5e3
+    Amars=4.*np.pi*Rmars**2
     t=4.5
     # model 1 - just use  dN20_dt1 - blue line  
     result = quad(dN20_dt1,0,t)
     
-    print('Model1: ' + str(result[0]))
+    print('Model1: ' + str(Amars*result[0]))
     
-    # model 2 - dN20_dt1 up to 3.5 and dN20_dt2 after - red line  
-    result = quad(dN20_dt1,0,3.5)
-    result2 = quad(dN20_dt2,3.5,t)
+    # model 2 - dN20_dt1 up to 4.1 and dN20_dt2 after - red line  
+    result = quad(dN20_dt1,0,4.1)
+    result2 = quad(dN20_dt2,4.1,t)
     
-    print('Model2: ' + str(result[0]+result2[0]))    
+    print('Model2: ' + str(Amars*(result[0]+result2[0])))    
     
-    # model 3 - dN20_dt1 up to 3.5 and dN20_dt3 after - red dashed line  
-    result = quad(dN20_dt1,0,3.5)
-    result2 = quad(dN20_dt3,3.5,t)
+    # model 3 - dN20_dt1 up to 4.1 and dN20_dt3 after - red dashed line  
+    result = quad(dN20_dt1,0,4.1)
+    result2 = quad(dN20_dt3,4.1,t)
     
-    print('Model3: ' + str(result[0]+result2[0]))        
+    print('Model3: ' + str(Amars*(result[0]+result2[0])))        
+    
+    
+    # test overall
+    result = quad(dN20_dt_integrand,0,t,2)
+    print('Overall: ' + str(Amars*result[0]))
+    
--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
+import crater_chronology
 
 """
     https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.odeint.html
@@ -18,6 +19,7 @@ if __name__ == "__main__":
     """
     Patm = 1.5 # initial pressure in most calculations - section 2.9, pp449
     tinit=-4.5e9 
+    Rmars=3389.5e3
     tfinal=0.
     tstep=1e6
     omega=2.*np.pi*1.e-9
@@ -30,6 +32,7 @@ if __name__ == "__main__":
     """
         auxiliary calcs ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     """
+    Amars=4.*np.pi*Rmars**2
     t = np.mgrid[tinit:tfinal+tstep:tstep]
     nsteps = len(t)
     n_ode=3
@@ -52,14 +55,11 @@ if __name__ == "__main__":
         main loop ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     """
     for i in range(nsteps-1):
-        if ((t[i]-last_output)>=output_interval):
-            print('time: ' + str(t[i]/1e9))
-            last_output = t[i]
                 
-        y0 = ystore[i,0:2]
         
         
         # solve ODEs
+        y0 = ystore[i,0:2]
         sol = odeint(test, y0, [t[i], t[i+1]], args=(omega,) )
         
         # 1. collapse? 0.5 bar and assumed to be 6 mbar
@@ -67,6 +67,8 @@ if __name__ == "__main__":
         # 2. calculate H2O - two temperatures
         
         # 3. impacts of asteroids and comets. during this time-step
+        num_impactor1=crater_chronology.N1_between(Amars,-t[i+1]/1e9,-t[i]/1e9)
+        num_impactor20=crater_chronology.N20_between(Amars,-t[i+1]/1e9,-t[i]/1e9,model=1)
         
         # 4. loss of the atmosphere following empirical formulae
         
@@ -82,6 +84,12 @@ if __name__ == "__main__":
         ystore[i+1,0]=sol[-1,0]
         ystore[i+1,1]=sol[-1,1]
         
+        if ((t[i]-last_output)>=output_interval):
+            print('time: ' + str(t[i]/1e9) + '; num impacts 1: ' + str(num_impactor1) + \
+                '; num impacts 20: ' + str(num_impactor20) + '; ratio: ' + \
+                 str(num_impactor1/num_impactor20) )
+            last_output = t[i]
+
     print('Final time: ' + str(t[-1]/1e9))
     """
         ----------------------------------------------------------------------------------
